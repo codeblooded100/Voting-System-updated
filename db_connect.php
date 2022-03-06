151e55269da623d9a0721f66906a4dff104cb470
oje <etuosborno@gmail.com>
2022-03-06 19:31:34 +0100

    make it better

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,7 @@
 <?php 
-
+/*
 $conn= new mysqli('localhost','root','','voting_db')or die("Could not connect to mysql".mysqli_error($con));
+*/
+$conn= new mysqli('eu-cdbr-west-02.cleardb.net','b742e9f74ab757','88908bf7','heroku_fdb1e9a6ee7e70a')or die("Could not connect to mysql".mysqli_error($con));
+
+?>
