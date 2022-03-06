18361c6b056b718f17ed3ae64a20dfb3b79d4c46
oje <etuosborno@gmail.com>
2022-03-06 16:57:33 +0100

    make it better

--- manage_voting.php
+++ manage_voting.php
@@ -1,16 +1,5 @@
 <?php include('db_connect.php');?>
 <?php
-/* heroku server*/ 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
 
 	$vote = $conn->query("SELECT * FROM voting_list where id=".$_GET['id']);
 	foreach ($vote->fetch_array() as $key => $value) {
