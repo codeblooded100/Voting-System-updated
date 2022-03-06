6d1a3ec6a0f26e757d84aedd6313ea2fc0173932
oje <etuosborno@gmail.com>
2022-03-06 16:38:18 +0100

    make it better

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-?>
+
 
 session_start();
 if(isset($_SESSION['login_id']))
